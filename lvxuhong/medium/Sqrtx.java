6a3f1a51b8fbad27260d68ed9f4b9fe57aa9b4cb
olive <adalxh@gmail.com>
2015-03-30 08:51:08 +0800

    2015/3/30

---
+++ lvxuhong/medium/Sqrtx.java
@@ -0,0 +1,15 @@
+package lvxuhong.medium;
+
+/**
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x.
+ * 
+ * @author xuhong
+ *
+ */
+public class Sqrtx {
+	public int sqrt(int x) {
+         return (int)Math.sqrt(x);
+	}
+}
