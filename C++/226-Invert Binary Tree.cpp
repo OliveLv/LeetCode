dd3d28444da3861a9d1d79c2359a25e72542ea68
OliveLv <adalxh@gmail.com>
2015-12-22 23:02:09 +0800

    指针，二叉树，后序遍历

---
+++ C++/226-Invert Binary Tree.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+using namespace std;
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)return NULL;
+		root->left=invertTree(root->left);
+		root->right=invertTree(root->right);
+		TreeNode* tmp=root->left;
+		root->left=root->right;
+		root->right=tmp;
+    }
+};
