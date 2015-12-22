1689bb8d5b50b8c6d4f3acb7a258e2418b6e84f8
olive <adalxh@gmail.com>
2015-12-22 18:11:26 +0800

    指针
    important

---
+++ C++/237-Delete Node in a Linked List.cpp
@@ -0,0 +1,37 @@
+#include<windows.h>
+using namespace std;
+// to deal with identifier 'NULL' is undefined
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+//class Solution {
+//public:
+//    void deleteNode(ListNode* node) {
+//		ListNode * p=node->next;
+//		while(p!=NULL){
+//			node->val=p->val;
+//			p=p->next;
+//			if(p==NULL)break;
+//			node=node->next;
+//		}
+//		node->next=NULL;
+//
+//    }
+//};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+		ListNode* p=node->next;
+		if(p!=NULL){
+		node->val=p->val;
+		node->next=p->next;
+		}
+		free(p);
+		
+
+    }
+};
