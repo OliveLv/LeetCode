870a09e6e9ef70cd8764173c4275adea60c2b5d8
olive <adalxh@gmail.com>
2015-12-22 18:10:56 +0800

    dfs，二叉树

---
+++ C++/104-Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+using namespace std;
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)return 0;
+		int l=maxDepth(root->left)+1;
+		int r=maxDepth(root->right)+1;
+		return l>r?l:r;
+    }
+};
