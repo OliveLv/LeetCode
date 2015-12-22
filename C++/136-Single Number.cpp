c43af80fb0a89221560f39e6c1414788ce98e995
olive <adalxh@gmail.com>
2015-12-22 18:11:12 +0800

    位运算（异或）

---
+++ C++/136-Single Number.cpp
@@ -0,0 +1,11 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+		for(int i=0;i<nums.size();i++)
+			res^=nums[i];
+		return res;
+    }
+};
