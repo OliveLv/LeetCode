141b6fefa7a5d1526c92bd315e6243ebb1a39866
OliveLv <adalxh@gmail.com>
2016-03-01 22:35:06 +0800

    剑指offer

---
+++ C++/169-Majority-Element.cpp
@@ -0,0 +1,22 @@
+﻿#include<vector>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        int pre=nums[0];
+        int cnt=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==pre)cnt++;
+            else
+                cnt--;
+            if(cnt==0){
+                pre=nums[i];
+                cnt=1;
+            }
+        }
+        return pre;
+    }
+
+};
